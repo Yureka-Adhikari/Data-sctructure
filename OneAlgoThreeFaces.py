dd9d30ee4abde28c0ced2cd436029d334ab27773
Yureka Adhikari <adhikariyureka@gmail.com>
2025-05-21 17:52:45 +0545

    first commit

---
+++ OneAlgoThreeFaces.py
@@ -0,0 +1,19 @@
+#First method
+
+def fun1(n):
+    return n*(n+1)/2
+
+print(fun1(4))
+
+#For function 1 algorithm goes like this : (4*5)/2
+
+#second method
+'''
+def fun2(n):
+    for i in range(1,n+1):
+        sum += 1
+    return sum
+print(fun2(4))
+'''
+#
+
